ae8923de772185b9ab264d79e9069788bed9b93d
AhmadAlhomsy <ahmadhoahmadho@gmail.com>
2026-06-17 14:48:23 +0300

    Initial commit: Add Two Sum solution

--- LeetCode/Easy/0001_Two_Sum.py
+++ LeetCode/Easy/0001_Two_Sum.py
@@ -2,9 +2,26 @@
 Problem: Two Sum
 Difficulty: Easy
 
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+
 Time Complexity: O(n)
 Space Complexity: O(n)
 
 Technique:
 Hash Map
 """
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
